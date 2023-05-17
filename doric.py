05002e02784eed3f3a03e96ecca13bb5a540c420
kkke <llkong.ck@gmail.com>
2023-05-17 17:09:36 -0400

    Update doric.py

--- doric.py
+++ doric.py
@@ -63,6 +63,38 @@ def ExtractDataAcquisition(filename):
 		#print(filename)
 		return h5getDatasetR(h['DataAcquisition'],filename)
 
+def extract_data_Ch2_OneSeries(folder, file_name):
+	data =[]
+	series = ['Series0001']
+	for ser in series:
+		dio01 = h5read(folder + file_name + '.doric',['DataAcquisition','FPConsole','Signals',ser,'DigitalIO','DIO01'])[0]
+		dio02 = h5read(folder + file_name + '.doric',['DataAcquisition','FPConsole','Signals',ser,'DigitalIO','DIO02'])[0]
+		dio03 = h5read(folder + file_name + '.doric',['DataAcquisition','FPConsole','Signals',ser,'DigitalIO','DIO03'])[0]
+		dio04 = h5read(folder + file_name + '.doric',['DataAcquisition','FPConsole','Signals',ser,'DigitalIO','DIO04'])[0]
+		dio_time = h5read(folder + file_name + '.doric',['DataAcquisition','FPConsole','Signals',ser,'DigitalIO','Time'])[0]
+		infusion = find_event(dio02, dio_time)
+		front    = find_event(dio03, dio_time)
+		back     = find_event(dio04, dio_time)
+		leverRetraction, leverInsertion = detect_edges(dio01, dio_time)
+		# Load from Channel 2: AOUT01 is reference, AOUT02 is signal
+		raw_reference = h5read(folder + file_name + '.doric',['DataAcquisition','FPConsole','Signals',ser,'AIN02xAOUT01-LockIn','Values'])[0]
+		raw_signal    = h5read(folder + file_name + '.doric',['DataAcquisition','FPConsole','Signals',ser,'AIN02xAOUT02-LockIn','Values'])[0]
+		# Load processed data
+		signal        = h5read(folder + file_name + '_DFF.doric',['DataProcessed','FPConsole','DFFSignals',ser,'AIN02xAOUT02-LockIn','Values'])[0]
+		time          = h5read(folder + file_name + '_DFF.doric',['DataProcessed','FPConsole','DFFSignals',ser,'AIN02xAOUT02-LockIn','Time'])[0]
+
+		data_series = {'infusion': infusion, 'front': front, 'back': back, 'leverInsertion': leverInsertion, 'leverRetraction': leverRetraction, 'raw_reference': raw_reference,
+					'raw_signal': raw_signal, 'signal': signal, 'time': time}
+		data.append(data_series)
+
+	all_data = []
+	for i in range(len(data)):
+		if i == 0:
+			all_data = data[i]
+		else:
+			for key in data[i]:
+				all_data[key] = np.concatenate((all_data[key], data[i][key]))
+	return data, all_data
 
 def extract_data_Ch2_ThreeSeries(folder, file_name):
 	data =[]
@@ -176,7 +208,7 @@ def psth_fb(data, time, event, pre, post, title_name):
     e = np.std(psth_signal, axis=1)/np.sqrt(psth_signal.shape[1])
     plt.fill_between(x, y-e, y+e)
     plt.xlabel('Time (s)')
-    plt.ylabel(r'z $\Delta$ F/F')
+    plt.ylabel(r'Z $\Delta$F/F')
     plt.xlim([np.min(np.mean(psth_time, 1)), np.max(np.mean(psth_time, 1))])
     # ylim([-0.1, 0.1])
     plt.ylim([-1.5, 1.5])
