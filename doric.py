9750ce066bd34a750d5457d28fdde5d704684768
kkke <llkong.ck@gmail.com>
2023-08-03 17:00:59 -0400

    Update doric.py

--- doric.py
+++ doric.py
@@ -121,14 +121,14 @@ def extract_data_Ch2_ThreeSeries(folder, file_name):
 					'raw_signal': raw_signal, 'signal': signal, 'time': time}
 		data.append(data_series)
 
-	all_data = []
-	for i in range(len(data)):
-		if i == 0:
-			all_data = data[i]
-		else:
-			for key in data[i]:
-				all_data[key] = np.concatenate((all_data[key], data[i][key]))
-	return data, all_data
+	# all_data = []
+	# for i in range(len(data)):
+	# 	if i == 0:
+	# 		all_data = data[i].copy()
+	# 	else:
+	# 		for key in data[i]:
+	# 			all_data[key] = np.concatenate((all_data[key], data[i][key]))
+	return data
 
 def extract_data_Ch4_OneSeries(folder, file_name):
 	data =[]
@@ -294,3 +294,48 @@ def psth_fb(data, time, event, pre, post, title_name):
     plt.subplots_adjust(hspace=0.5)
     plt.show()
     return psth_time, psth_signal, fig
+
+def smooth_signal(x,window_len=10,window='flat'):
+
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    The code taken from: https://scipy-cookbook.readthedocs.io/items/SignalSmooth.html
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+                'flat' window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal        
+    """
+
+    import numpy as np
+
+    if x.ndim != 1:
+        raise(ValueError, "smooth only accepts 1 dimension arrays.")
+
+    if x.size < window_len:
+        raise(ValueError, "Input vector needs to be bigger than window size.")
+
+    if window_len<3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise(ValueError, "Window is one of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+
+    if window == 'flat': # Moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+
+    return y[(int(window_len/2)-1):-int(window_len/2)]
